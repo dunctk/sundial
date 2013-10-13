fe07f51a5b2360e234ff4b313e11e30fc7db40f9
Duncan Trevithick <duncan.trevithick@gmail.com>
2013-10-13 22:25:40 +0100

    Added calculation for determining sun graphic position for a given angle

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+function getSunset() {
+	
+};
+
+function getSunPosition() {
+	center_x = 300;
+	center_y = 300;
+	radius = 200;
+	angle = 0;
+	x = center_x + radius * Math.cos(angle * (Math.PI / 180));
+	y = center_y + radius * Math.sin(angle * (Math.PI / 180));
+	return [x,y];
+};
+
+//Setup canvas
 var primary_canvas = document.getElementById("primary");
 var context = primary_canvas.getContext("2d");
 
+//Draw outer circle
 context.beginPath();
-context.arc(95, 85, 40, 0, 2*Math.PI);
+context.arc(300, 300, 200, 0, 2*Math.PI);
 context.closePath();
 context.lineWidth = 2;
-context.strokeStyle = "#f0e6a3"
+context.strokeStyle = "#f0e6a3";
 context.stroke();
+
+//Draw sun 
+var sun_position = getSunPosition();
+context.beginPath();
+context.arc(sun_position[0], sun_position[1], 30, 0, 2*Math.PI);
+context.closePath();
+context.fillStyle = "#f0e6a3";
+context.fill();
