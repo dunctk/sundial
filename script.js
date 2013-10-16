c1d98e7d2525933df77da98b4e58ec102cba0b7b
Duncan Trevithick <duncan.trevithick@gmail.com>
2013-10-16 15:25:12 +0100

    Using kineticjs

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+DIAL_CENTER = [300,300];
+DIAL_RADIUS = 200;
+
 function getSunset() {
 	
 };
 
 function getSunPosition() {
-	center_x = 300;
-	center_y = 300;
-	radius = 200;
-	angle = 0;
-	x = center_x + radius * Math.cos(angle * (Math.PI / 180));
-	y = center_y + radius * Math.sin(angle * (Math.PI / 180));
+	var time = new Date();
+	var angle = (time.getHours() * 15) + 270;
+	x = DIAL_CENTER[0] + DIAL_RADIUS * Math.cos(angle * (Math.PI / 180));
+	y = DIAL_CENTER[1] + DIAL_RADIUS * Math.sin(angle * (Math.PI / 180));
 	return [x,y];
 };
 
@@ -16,18 +17,37 @@ function getSunPosition() {
 var primary_canvas = document.getElementById("primary");
 var context = primary_canvas.getContext("2d");
 
-//Draw outer circle
-context.beginPath();
-context.arc(300, 300, 200, 0, 2*Math.PI);
-context.closePath();
-context.lineWidth = 2;
-context.strokeStyle = "#f0e6a3";
-context.stroke();
+function drawDial() {
+	//Draw the daytime part of the dial
+	context.beginPath();
+	context.arc(DIAL_CENTER[0], DIAL_CENTER[1], DIAL_RADIUS, 1, 1.9*Math.PI);
+	//context.closePath();
+	context.lineWidth = 2;
+	context.strokeStyle = "#f0e6a3";
+	context.stroke();
 
-//Draw sun 
-var sun_position = getSunPosition();
-context.beginPath();
-context.arc(sun_position[0], sun_position[1], 30, 0, 2*Math.PI);
-context.closePath();
-context.fillStyle = "#f0e6a3";
-context.fill();
+	//Draw the nightime part of the dial
+	context.beginPath();
+	context.arc(DIAL_CENTER[0], DIAL_CENTER[1], DIAL_RADIUS, 0.6, 0.8*Math.PI);
+	context.lineWidth = 3;
+	context.strokeStyle = "blue";
+	context.stroke();
+};
+
+function drawSun() {
+	var sun_position = getSunPosition();
+	context.beginPath();
+	context.arc(sun_position[0], sun_position[1], 30, 0, 2*Math.PI);
+	context.closePath();
+	context.fillStyle = "#f0e6a3";
+	context.fill();
+	//t = setTimeout(function(){drawSun()}, 100);
+};
+
+//Draw text
+var time = new Date();
+var h = time.getHours();
+var m = time.getMinutes();
+context.font = "20px Helvetica Neue";
+context.fillStyle = "#fff";
+
