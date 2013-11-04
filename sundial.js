6e39c3241bf22bcf6cf7b5fa6cd9c5c3749ec3c3
Duncan Trevithick <duncan.trevithick@gmail.com>
2013-11-04 17:49:44 +0000

    Fixed calculation of the daylight part of the dial

--- sundial.js
+++ sundial.js
@@ -5,12 +5,10 @@
 
 var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
 
-
 // format sunrise time from the Date object
 
 var sunriseStr = moment(times.sunrise).format('HH:mm');
 var sunsetStr = moment(times.sunset).format('HH:mm');
-
 
 //Get the user's longitude
 
@@ -18,6 +16,15 @@ navigator.geolocation.getCurrentPosition(GetLongitude);
 function GetLongitude(location) {
     return(location.coords.latitude);
 }
+
+function IsNightTime(){
+	var current_time = new Date();
+	if (current_time > times.sunset){
+		return true;
+	} else {
+		return false;
+	};
+};
 
 
 //Set the stage
@@ -50,20 +57,16 @@ var dialDaytime = new Kinetic.Shape ({
 	stroke: '#f0e6a3',
 	strokeWidth: 3,
 	drawFunc: function(context) {
-		//One minute on the dial, in radians
-		var r = 0.00432;
 		var riseTime = times.sunrise;
 		var setTime = times.sunset;
-		var riseAngle = (getDialAngle(riseTime) * r);
-		var setAngle = (getDialAngle(setTime) * r * Math.PI);
-		console.log(setAngle);
-		console.log(riseAngle);
+		var riseAngle = (getDialAngle(riseTime) * (Math.PI / 180));
+		var setAngle = (getDialAngle(setTime) * (Math.PI / 180));
 		context.beginPath();
 		context.arc(0, 0, 250, riseAngle, setAngle);
 		context.fillStrokeShape(this);
 	}
 });
-dialDaytime.rotate(Math.PI * 0.5);
+//dialDaytime.rotate(Math.PI * 0.5);
 
 // The 'night' part of the dial
 
@@ -125,12 +128,6 @@ var dialLocationText = new Kinetic.Text({
     text: 'London, UK'
 });
 	
-function getSunset() {
-	var sunset = new Date ();
-	//Placeholder data
-	sunset.setDate(2013, 9, 17, 8, 0, 0);
-	return(sunset);
-};
 function getX(time) {
 	var angle = getDialAngle(time);
 	var x = dialBase.getX() + dialBase.getRadius()  * Math.cos(angle * (Math.PI / 180));
@@ -142,13 +139,35 @@ function getY(time){
 	return y;
 };
 
-var sun_layer = new Kinetic.Layer();
-var sun_circle = new Kinetic.Circle({
+var celestial_layer = new Kinetic.Layer();
+var sun = new Kinetic.Circle({
 	x: getX(current_time),
 	y: getY(current_time),
 	radius: 30,
 	fill: '#f0e6a3',
 	draggable: false
+});
+
+var moon = new Kinetic.Circle({
+	x: getX(current_time),
+	y: getY(current_time),
+	radius: 30,
+	strokeWidth: 2,
+	stroke: '#E6E6E6'
+});
+
+
+var test_layer_sunrise = new Kinetic.Circle({
+	x: getX(times.sunrise),
+	y: getY(times.sunrise),
+	radius: 5,
+	fill: 'red'
+});
+var test_layer_sunset = new Kinetic.Circle({
+	x: getX(times.sunset),
+	y: getY(times.sunset),
+	radius: 5,
+	fill: 'red'
 });
 
 
@@ -162,13 +181,19 @@ dialLayer.add(dialLocationText);
 dialLayer.add(dialTimeText);
 dialLayer.add(dialSunriseTimeText);
 dialLayer.add(dialSunsetTimeText);
-dialLayer.add(dialBase);		
+dialLayer.add(dialBase);	
+
 stage.add(dialLayer);
-sun_layer.add(sun_circle);
-stage.add(sun_layer);
+
+//Todo: Decide whether to show the moon or sun
+if (IsNightTime()) {
+	celestial_layer.add(moon);
+} else {
+	celestial_layer.add(sun);
+}
+
+stage.add(celestial_layer);
 onUpdateTimeText();
-
-
 
 
 //Set time to keep the dial time updated
@@ -183,8 +208,8 @@ function onUpdateTimeText() {
 setInterval(function() {onUpdateSunPosition() }, 30000);
 function onUpdateSunPosition() {
 	var current_time = new Date();
-	sun_circle.setX(getX(current_time));
-	sun_circle.setY(getY(current_time));
-	sun_layer.draw();
+	sun.setX(getX(current_time));
+	sun.setY(getY(current_time));
+	celestial_layer.draw();
 }
 
